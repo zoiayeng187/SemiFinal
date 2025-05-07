b3b5273d0a5f17aad173cc5074be2ece37846270
zoiayeng187 <invokerkael14@gmail.com>
2025-05-07 23:53:46 +0800

    Updated port binding for Railway deployment

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ def home():
     return "Ayeng, Zoi Artchelo L. BSIT-||| B "
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT",5000))
+    port = int(os.environ.get("PORT", 8080))  # Default to 8080 if PORT isn't set
     app.run(host='0.0.0.0', port=port)
